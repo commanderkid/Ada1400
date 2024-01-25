401beb498e856e85051fc033d1dbc361aa247109
Alexey <nafiguspofigus@gmail.com>
2024-01-25 11:09:46 +0300

    Delete AdaLearn/NumbersUnderEachOther.adb
    deleted

--- AdaLearn/NumbersUnderEachOther..adb
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; 
-
-procedure NumbersUnderEachOther is
-    package OiOi renames Ada.Text_IO;
-begin
-    OiOi.Put_Line ("1");
-    OiOi.Put_Line ("2");
-    OiOi.Put_Line ("3");
-end NumbersUnderEachOther;
